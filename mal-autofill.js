c73bc8932d3a240f1f88cc4d6d40bea46ec4dd5e
Sophia <sophia97@my.yorku.ca>
2018-04-20 13:46:26 -0400

    added version

--- mal-autofill.js
+++ mal-autofill.js
@@ -5,7 +5,7 @@
 // @include     *animebytes.tv/upload.php*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant       GM.xmlHttpRequest
-// @version     1
+// @version     2018.04.20
 // ==/UserScript==
 //jshint esversion: 6
 
